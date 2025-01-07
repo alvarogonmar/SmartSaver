1eb9877c11877acc8441871b5ca4d3bec9e578b0
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-06 22:42:40 -0600

    add budget reducer with initial state and action types

---
+++ smart-saver-contextapi/src/reducers/budget-reducer.ts
@@ -0,0 +1,18 @@
+
+export type BudgetActions =
+    { type: "add-budget", payload: {budget: number} }
+
+export type BudgetState = {
+    budget: number
+}
+
+export const initialState : BudgetState ={
+    budget: 0
+}
+
+export const budgetReducer = (
+        state: BudgetState = initialState,
+        action: BudgetActions
+    ) => {
+
+}
