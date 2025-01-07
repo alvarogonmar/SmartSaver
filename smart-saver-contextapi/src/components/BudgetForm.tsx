0262a29d157f1286cae09ac9ce687f3752bfd39e
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-06 21:46:10 -0600

    enhance BudgetForm component with form structure and styling

--- smart-saver-contextapi/src/components/BudgetForm.tsx
+++ smart-saver-contextapi/src/components/BudgetForm.tsx
@@ -1,3 +1,22 @@
 export default function BudgetForm() {
-  return <div>BudgetForm</div>;
+  return (
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-4l text-blue-600 font-bold text-center"
+        >
+          Define Budget
+        </label>
+        <input
+          id="budget"
+          type="number"
+          className="w-full bg-white border border-gray-200 p-2"
+          placeholder="Enter your budget"
+          name="budget"
+        />
+      </div>
+      <input />
+    </form>
+  );
 }
