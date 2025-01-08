5a8f09168ebc7705c3ba84e16587c44f442d6206
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-07 20:45:56 -0600

    enhance ExpenseForm component with new expense input form

--- smart-saver-contextapi/src/components/ExpenseForm.tsx
+++ smart-saver-contextapi/src/components/ExpenseForm.tsx
@@ -1,3 +1,22 @@
 export default function ExpenseForm() {
-  return <div>ExpenseForm</div>;
+  return (
+    <form className="space-y-5">
+      <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
+        New Expense
+      </legend>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="expenseName" className="text-xl font-bold ">
+          New Expense:
+        </label>
+        <input
+          type="text"
+          id="expenseName"
+          placeholder="Enter your new expense"
+          className="bg=slate-100 p-2"
+          name="expenseName"
+        />
+      </div>
+    </form>
+  );
 }
