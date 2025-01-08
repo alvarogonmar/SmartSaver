5d4aef81ed9b7bbccd99db2ab2c1aa183d653bcc
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-07 19:25:18 -0600

    use formatCurrency helper in AmountDisplay component for consistent amount formatting

--- smart-saver-contextapi/src/components/AmountDisplay.tsx
+++ smart-saver-contextapi/src/components/AmountDisplay.tsx
@@ -1,3 +1,5 @@
+import { formatCurrency } from "../helpers";
+
 type AmountDisplayProps = {
   label: string;
   amount: number;
@@ -7,7 +9,7 @@ export default function AmountDisplay({ label, amount }: AmountDisplayProps) {
   return (
     <p className="text-2xl text-blue-600 font-bold">
       {label}: {""}
-      <span className="font-black text-black">{amount}</span>
+      <span className="font-black text-black">{formatCurrency(amount)}</span>
     </p>
   );
 }
