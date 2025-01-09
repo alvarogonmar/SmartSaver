1dde01416256ba5d8f4571268a8fa203a4679a5f
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-08 21:45:22 -0600

    feat: add category name display to ExpenseDetail component using useMemo

--- smart-saver-contextapi/src/components/ExpenseDetail.tsx
+++ smart-saver-contextapi/src/components/ExpenseDetail.tsx
@@ -1,17 +1,27 @@
 import { Expense } from "../types";
 import { formatDate } from "../helpers";
 import AmountDisplay from "./AmountDisplay";
+import { useMemo } from "react";
+import { categories } from "../data/categories";
 
 type ExpenseDetailProps = {
   expense: Expense;
 };
 
 export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
+  const categoryInfo = useMemo(
+    () => categories.filter((cat) => cat.id === expense.category)[0],
+    [expense]
+  );
+
   return (
     <div className="bg-white shadowlg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
       <div></div>
 
       <div>
+        <p className="text-sm font-bold uppercase text-slate-500">
+          {categoryInfo.name}
+        </p>
         <p>{expense.expenseName}</p>
         <p className="text-slate-600 text-sm">
           {formatDate(expense.date!.toString())}
