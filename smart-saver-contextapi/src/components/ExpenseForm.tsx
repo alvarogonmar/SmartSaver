fa3e6db34d9fba85c6fe563148481ec88ec51a1a
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-08 18:07:42 -0600

    feat: initialize state for expense form with default values

--- smart-saver-contextapi/src/components/ExpenseForm.tsx
+++ smart-saver-contextapi/src/components/ExpenseForm.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import type { DraftExpense } from "../types";
 import { categories } from "../data/categories";
 import DatePicker from "react-date-picker";
 import "react-date-picker/dist/DatePicker.css";
 import "react-calendar/dist/Calendar.css";
 
 export default function ExpenseForm() {
+  const [expense, setExpense] = useState<DraftExpense>({
+    amount: 0,
+    expenseName: "",
+    category: "",
+    date: new Date(),
+  });
+
   return (
     <form className="space-y-5">
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
