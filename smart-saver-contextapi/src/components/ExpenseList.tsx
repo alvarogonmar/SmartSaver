ce6536100258151b9b18a6879aa60ca68322c830
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-08 20:51:29 -0600

    feat: enhance ExpenseList component to display message when no expenses are present

--- smart-saver-contextapi/src/components/ExpenseList.tsx
+++ smart-saver-contextapi/src/components/ExpenseList.tsx
@@ -1,3 +1,20 @@
+import { useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
+
 export default function ExpenseList() {
-  return <div>ExpenseList</div>;
+  const { state } = useBudget();
+
+  const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses]);
+
+  return (
+    <div className="mt-10">
+      {isEmpty ? (
+        <p className="text-gray-600 text-2xl font-bold">No Expenses yet</p>
+      ) : (
+        <>
+          <p className="text-gray-600 text-2xl font-bold my-5">Expense List</p>
+        </>
+      )}
+    </div>
+  );
 }
