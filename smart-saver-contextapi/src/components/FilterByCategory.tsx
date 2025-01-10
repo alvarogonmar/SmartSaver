2e122bcb53cdb379c53365d90fbfaf21ba6dca22
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-09 21:45:36 -0600

    feat: implement category filtering in FilterByCategory component

--- smart-saver-contextapi/src/components/FilterByCategory.tsx
+++ smart-saver-contextapi/src/components/FilterByCategory.tsx
@@ -1,5 +1,21 @@
+import { categories } from "../data/categories";
+
 export default function FilterByCategory() {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-10">FilterByCategory</div>
+    <div className="bg-white shadow-lg rounded-lg p-10">
+      <form>
+        <div className="flex flex-col md:flex-row md:items-center">
+          <label htmlFor="category">Filter expenses ㅤ</label>
+          <select id="category" className="bg-slate-100 p-3 flex-1 rounded">
+            <option value="">- - All Categories - -</option>
+            {categories.map((category) => (
+              <option value={category.id} key={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </form>
+    </div>
   );
 }
